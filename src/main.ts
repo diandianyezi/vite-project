5ba505cbffc8931aa3eeceeb02e936d642301c5a
zhangting5 <zhangting5@xiaomi.xom>
2022-07-22 18:38:01 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './assets/css/reset.less'
 import Loading from './plugins/Loading'
 import { createPinia } from 'pinia'
+// import vueRouter from 'vue-router'
 
 const store = createPinia()
 
@@ -19,5 +20,6 @@ declare module '@vue/runtime-core' {
 app.use(Loading)
 
 app.use(store)
+// app.use(vueRouter)
 
 app.mount('#app')
